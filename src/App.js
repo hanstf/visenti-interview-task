d0715baf447567346aa61bb450ade36c7dcd94b6
hanstf <hansbrians@gmail.com>
2017-08-21 04:21:38 +0800

    init dygraph components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import Dygraph from 'dygraphs';
 import './App.css';
+import * as data from '../sample-data.json' ;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-        </div>
-        <p className="App-intro">
-        </p>
-      </div>
-    );
+    constructor (props) {
+        super(props);
+        this.state = {
+            config: {
+                "title": "Pressure Transient(s)",
+                "xlabel": "Time",
+                "ylabel": "Pressure (meters)",
+                "gridLineWidth": 0.1,
+                "labels": ["Date", "Tampines Ave10 (Stn 40)"],
+                "connectSeparatedPoints": true,
+                "axes": {
+                    "x": {
+                        "axisLabelFontSize": 9
+                    }
+                }
+            },
+            data: function () {
+                var arr = [];
+                for (var key in data.default) {
+                    var eachData = data.default[key];
+                    eachData[0] = new Date(eachData[0]);
+                    arr.push(eachData);
+                }
+                return arr;
+            }
+        };
+    }
+  componentDidMount() {
+      var data = this.state.data();
+      new Dygraph(
+          document.getElementsByClassName("App-body")[0],
+          data,
+          this.state.config
+      );
   }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-body">
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
