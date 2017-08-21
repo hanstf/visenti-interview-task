4d76784b2081d239cfb60ed1cbe7ae0d2e6fcf63
hanstf <hansbrians@gmail.com>
2017-08-21 16:36:46 +0800

    create graph own component

--- src/Graph.js
+++ src/Graph.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Dygraph from 'dygraphs';
-import './App.css';
+import './Graph.css';
 import * as data from '../sample-data.json' ;
 
 class App extends Component {
@@ -18,7 +18,8 @@ class App extends Component {
                     "x": {
                         "axisLabelFontSize": 9
                     }
-                }
+                },
+                "legend": "always"
             },
             data: function () {
                 var arr = [];
@@ -31,18 +32,18 @@ class App extends Component {
             }
         };
     }
-  componentDidMount() {
-      var data = this.state.data();
-      new Dygraph(
-          document.getElementsByClassName("App-body")[0],
-          data,
-          this.state.config
-      );
-  }
+    componentDidMount() {
+        var data = this.state.data();
+        new Dygraph(
+            document.getElementsByClassName("Graph-body")[0],
+            data,
+            this.state.config
+        );
+    }
     render() {
         return (
-            <div className="App">
-                <div className="App-body">
+            <div className="Graph">
+                <div className="Graph-body">
                 </div>
             </div>
         );
